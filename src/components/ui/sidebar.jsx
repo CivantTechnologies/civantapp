4c5b6e0b2a4a9b5bebd00338c3f2c31fa0009347
CivantTechnologies <258097577+CivantTechnologies@users.noreply.github.com>
2026-02-28 10:53:16 +0000

    Sidebar: no scroll at all (overflow-hidden)

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -337,7 +337,7 @@ const SidebarContent = React.forwardRef(({ className, ...props }, ref) => {
       ref={ref}
       data-sidebar="content"
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-2 overflow-y-auto overflow-x-hidden group-data-[collapsible=icon]:overflow-hidden",
+        "flex min-h-0 flex-1 flex-col gap-2 overflow-hidden group-data-[collapsible=icon]:overflow-hidden",
         className
       )}
       {...props} />)
